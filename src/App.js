233853d3c568917f6f91d1647269bb843586f870
piyush <piyushthakare999@gmail.com>
2023-08-24 00:07:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
-
+import react, { useState } from 'react';
 function App() {
+  const [code, setCode] = useState('');
+  const [language,setLanguage]= useState("cpp");
+  const [output, setOutput] = useState("");
+
+  const handleSubmit = async () => {
+    const payload = {
+      language,
+      code
+    };
+    try {
+      const { data } = await axios.post("http://localhost:5000/run", payload);
+      setOutput(data.output);
+    }catch({response}){
+      if(response){
+        const errMsg=response.data.err.stderr;
+        console.log(response);
+      }
+      else{
+        window.alert("error connecting server!")
+      }
+      // console.log(err.response)
+    }
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>online code compiler</h1>
+      <div>
+        <label htmlFor="">Language: </label>
+      <select name="" id="" value={language}
+      onChange={(e)=>{
+        setLanguage(e.target.value);
+        console.log(e.target.value);
+      }}>
+        <option value="cpp">C++</option>
+        <option value="py">Python</option>
+      </select>
+      </div>
+      <br />
+      <textarea name="" id="" cols="80" rows="20" value={code} onChange={(e) => {
+        setCode(e.target.value);
+      }}></textarea>
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+      <p>{output}</p>
     </div>
   );
 }
